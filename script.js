474cc604b1c3914c30afc40a0df5e79379a81d0e
pdriding <peterdriding@gmail.com>
2023-04-04 13:15:55 +0400

    Implemented game logic

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number ⛔';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct 🎉';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
